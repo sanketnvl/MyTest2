3c9655e3b6d712219aaafd4d42b8837ee18609dc
Sanket Navale <sanketnvl@gmail.com>
2014-05-12 12:59:15 +0530

    My first changes

---
+++ src/com/myexamples/Welcome.java
@@ -0,0 +1,14 @@
+package com.myexamples;
+
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome to my home ");
+
+	}
+
+}
